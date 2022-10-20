9848c8f7d551b6940b985976aee18684685a0e47
oumbarae hachmahocine <hachmaoui@hotmail.fr>
2022-10-20 09:21:34 +0200

    phase 2 : 1ere test 'Hello world ! ' .

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Description of AccueilController
+ *
+ * @author hachm
+ */
+class AccueilController {
+    //put your code here
+    
+    /**
+     * @Route("/",name="accueil")
+     * @return Response
+     */
+    public function index() : Response {
+        return new Response ('Hello world ! ');
+        
+    }
+}
